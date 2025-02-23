eb32e016ee67ff53ba2a6de5fe28c794ed3ac2b5
tsu <smthanh@gmail.com>
2025-02-23 23:46:56 +0700

    fix build

--- frontend/src/components/ImageUpload.tsx
+++ frontend/src/components/ImageUpload.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useMemo } from "react";
 import { useDropzone } from "react-dropzone";
 import { toast } from "react-hot-toast";
-import ScreenRecorder from "./recording/ScreenRecorder";
+// import ScreenRecorder from "./recording/ScreenRecorder";
 import { ScreenRecorderState } from "../types";
 
 const baseStyle = {
